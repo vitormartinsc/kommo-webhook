2ee140b10b979e2f56f7c3d389b117d6727b8bdf
vitormartinsc <vitormartinscarvalho@gmail.com>
2025-03-31 19:48:07 -0300

    Second Test

--- index.js
+++ index.js
@@ -28,16 +28,19 @@ app.post("/webhook", (req, res) => {
 
   const valorSaque = (limite / 1.56).toFixed(2); // Exemplo com taxa de 56%
   const valorParcela = (valorSaque / 12).toFixed(2); // 12x como exemplo
-
-  res.json({
-    show: {
-      type: "text",
-      value: `💳 Limite disponível: R$ ${limite.toLocaleString("pt-BR")}
-📆 12x de R$ ${valorParcela} = R$ ${valorSaque} de saque aproximado.
-
-Esta é a melhor opção em custo-benefício, ${nome}. Vamos avançar?`
+  res.json([
+    {
+      handler: "show",
+      params: {
+        type: "text",
+        value: `💳 Limite disponível: R$ ${limite.toLocaleString("pt-BR")}
+  📆 12x de R$ ${valorParcela} = R$ ${valorSaque} de saque aproximado.
+  
+  Esta é a melhor opção em custo-benefício, ${nome}. Vamos avançar?`
+      }
     }
-  });
+  ]);
+  
 });
 
 // Inicialização do servidor
