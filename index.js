0af9a66d1d1dbcb07cf9fc696e364216c9e9fc00
vitormartinsc <vitormartinscarvalho@gmail.com>
2025-04-08 18:12:31 -0300

    adding ngrok url

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+let ngrokUrl = process.env.NGROK_URL || ""; // fallback
 
 // Middleware
 app.use(cors());
